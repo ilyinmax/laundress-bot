4b64169579437d78341f7f0bcb1864678f233ee0
Maxim <ilyinmv2005@gmail.com>
2026-08-18 21:14:10 +0300

    Явно подключить команды управления админами в polling

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from config import BOT_TOKEN, WASHING_MACHINES, DRYERS
 from database import init_db, add_machine, get_machines_by_type
 from scheduler import setup_scheduler, schedule_reminder
 
-from handlers import registration, booking, admin_extra, admin
+from handlers import registration, booking, admin_access, admin_extra, admin
 from handlers.bot_commands import setup_bot_commands
 from database import init_db
 init_db()
@@ -26,6 +26,8 @@ async def main():
 
     dp.include_router(registration.router)
     dp.include_router(booking.router)
+    # Управление админами подключаем явно, без побочных эффектов импорта.
+    dp.include_router(admin_access.router)
     # Новая админ-панель должна идти раньше старого admin.py,
     # чтобы перехватить /admin и добавить раннюю запись, не ломая старые callback'и.
     dp.include_router(admin_extra.router)
